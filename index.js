12e048f3a740c55cf5556fbef5a9b622d49135ce
eman obeidat <amoonobeidat43@gmail.com>
2023-04-16 09:21:08 +0000

    D

--- index.js
+++ index.js
@@ -47,13 +47,13 @@ function getHomeHandler(req, res) {
 }
 
 function filterHandler(req, res) {
-  let price=req.query.price;
+  let price = req.query.price;
   let city = req.query.city;
   price = price.split('-');
   firstPrice = price[0].slice(0, price[0].length - 1);
-  firstPrice = parseInt(firstPrice)*1000;
+  firstPrice = parseInt(firstPrice) * 1000;
   secondPrice = price[1].slice(0, price[1].length - 1);
-  secondPrice = parseInt(secondPrice)*1000;
+  secondPrice = parseInt(secondPrice) * 1000;
   let array = [];
   for (let i = 0; i < data.length; i++) {
     const element = data[i];
@@ -98,6 +98,51 @@ function error404(req, res) {
 function errorHandler(err, req, res) {
   return res.status(500).json({ status: 500, responseText: "ERROR 500" });
 }
+
+
+
+
+// //loginValidateForm
+app.get('/valid', validateForm)
+function validateForm() {
+  // Get the value of the input field with id="email"
+  var email = document.getElementById("email").value;
+  // Get the value of the input field with id="password"
+  var password = document.getElementById("password").value;
+  // Regular expression to check if the email is in the correct format
+  var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  // Check if the email is empty
+  if (email == "") {
+    alert("Email field must be filled out");
+    return false;
+  }
+  // Check if the email is in the correct format
+  if (!email.match(emailRegex)) {
+    alert("Please enter a valid email address");
+    return false;
+  }
+  // Check if the password is empty
+  if (password == "") {
+    alert("Password field must be filled out");
+    return false;
+  }
+}
+
+//loginVerification
+app.get('/verfication',loginVerification)
+function loginVerification() {
+  const userName = req.query.userName;
+  const passWord = req.query.passWord;
+  if (userName == userName && passWord == passWord) {
+    res.send("login successfully")
+  }
+  else 
+  {
+    res.send("Please ! Enter valid UserName Or Password")
+  }
+}
+
+
 
 
 app.get('/', (req, res) => {
